5aeaf7387ea9b8997e5a871b1d1eaddc3acf9cc5
Vexus1 <adriangalik123@gmail.com>
2023-07-15 20:24:59 +0200

    task 13, 217

--- 217_contains_duplicate.py
+++ 217_contains_duplicate.py
@@ -1,7 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
         hashset = set()
-
         for n in nums:
             if n in hashset:
                 return True
