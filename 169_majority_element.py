4ac313c0cb02a00695b56135d0e595050351667f
Vexus1 <adriangalik123@gmail.com>
2023-07-16 21:15:31 +0200

    169 task another solution by Boyer–Moore majority vote algorithm

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -3,7 +3,6 @@ import collections
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
         dict_ = collections.defaultdict(int)
-
         for i in nums:
             dict_[i] +=1
 
@@ -11,6 +10,25 @@ class Solution:
         for key, value in dict_.items():
             if value > n:
                 return key
+        
+        return 0
             
+solution = Solution()
+print(solution.majorityElement([3,3,4]))
+
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        count = 0
+        candidate = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
+    
 solution = Solution()
 print(solution.majorityElement([3,3,4]))
