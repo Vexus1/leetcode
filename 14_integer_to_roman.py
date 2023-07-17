5517d6d4c2ff7ce69fdb35b449c88db8cd66bb43
Vexus1 <adriangalik123@gmail.com>
2023-07-17 20:43:09 +0200

    14 task

---
+++ 14_integer_to_roman.py
@@ -0,0 +1,11 @@
+from math import floor
+class Solution:
+    def intToRoman(self, num):
+        ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        hrns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        ths =  ["", "M", "MM", "MMM"]
+
+        return ths[floor(num / 1000)] + hrns[floor((num % 1000) / 100)] + tens[floor((num % 100) / 10)] + ones[floor(num % 10)]
+solution = Solution()
+print(solution.intToRoman(num = 58))
