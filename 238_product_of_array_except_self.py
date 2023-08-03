421fc60a60054b2701e4905a2d36f8d8cf8dd143
Vexus1 <adriangalik123@gmail.com>
2023-08-03 15:50:41 +0200

    238 task

--- 238_product_of_array_except_self.py
+++ 238_product_of_array_except_self.py
@@ -1,10 +1,18 @@
 from collections import defaultdict
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        dict_ = defaultdict(int)
-        for n in nums:
-            dict_[n] += 1
-        
+        len_nums = len(nums)
+        result = [1] * len_nums
+        prefix = 1
+        for i in range(len_nums):
+            result[i] = prefix
+            prefix *= nums[i]
+        suffix = 1
+        for i in range(len_nums-1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
+        return result
+
 sol = Solution()
-print(sol.productExceptSelf(nums = [1,2,3,4]))
+print(sol.productExceptSelf(nums = [1,2,3,4])) # -> [24,12,8,6]
 print(sol.productExceptSelf(nums = [-1,1,0,-3,3]))
