114361e4024ca95eb8917f56f460c392e411e448
Adrian Galik <adriangalik123@gmail.com>
2024-01-18 13:48:32 +0100

    task 15 default dict approach

--- 15_3sum.py
+++ 15_3sum.py
@@ -1,3 +1,4 @@
+# two pointers approach
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
@@ -27,6 +28,47 @@ class Solution:
         return list(list(i) for i in result)
 
             
+
+sol = Solution()
+print(sol.threeSum(nums = [-1,0,1,2,-1,-4]))
+print(sol.threeSum(nums = []))
+print(sol.threeSum(nums = [0,0,0]))
+
+
+# default dict approach
+from collections import defaultdict
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        positives = defaultdict(int)
+        negatives = defaultdict(int)
+        zeros = 0
+        for n in nums:
+            if n > 0:
+                positives[n] += 1
+            elif n < 0:
+                negatives[n] += 1
+            else:
+                zeros += 1
+        
+        result = []
+        if zeros:
+            for n in negatives.keys():
+                if -n in positives.keys():
+                    result.append([0, n, -n])
+            if zeros > 2:
+                result.append([0,0,0])
+        
+        for set1, set2 in ((negatives, positives), (positives, negatives)):
+            set1_items = list(set1.items())
+            for i, (j, k) in enumerate(set1_items):
+                for j2, _ in set1_items[i:]:
+                    if j != j2 or (j == j2 and k > 1):
+                        if -j-j2 in set2:
+                            result.append([j, j2, -j-j2])
+        return result
+
+
 
 sol = Solution()
 print(sol.threeSum(nums = [-1,0,1,2,-1,-4]))
